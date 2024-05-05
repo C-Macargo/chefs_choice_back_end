b7d588632ec0e64546d423fdd803bb473d2226d6
C-Macargo <caio.camargos97@gmail.com>
2024-05-04 22:55:31 -0300

    fix: eslint line size

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     'semi': ['error', 'always'], 
-    'max-len': ['error', { 'code': 120 }],
+    'max-len': ['error', { 'code': 160 }],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
